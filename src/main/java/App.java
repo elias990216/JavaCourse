cd78ac3dcf86de0b0713d75ebd14995558c100b8
Elias Muñoz <emunozp@minsait.com>
2022-09-12 11:04:31 -0500

    Java Course

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class App {
     public static void main(String[] args) {
-
+        List<Integer> integerList = Arrays.asList(10,4,5,6,7);
+        Map<String, Integer> mapTest = integerList.stream().collect(Collectors.toMap(e->e+"",e->e));
+        System.out.println(mapTest);
     }
 }
