543c6ab2ae85bd3105b0e75cca9aaa765dbc3d64
Elias Muñoz <emunozp@minsait.com>
2022-09-12 11:33:40 -0500

    Course 12-Sep-22

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,15 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(10,4,5,6,7);
-        Map<String, Integer> mapTest = integerList.stream().collect(Collectors.toMap(e->e+"",e->e));
+        Map<String, Integer> mapTest = integerList.stream().collect(Collectors.toMap(e->e+"", Function.identity()));
         System.out.println(mapTest);
+        List<String> listTest = mapTest.keySet().stream().toList();
+        System.out.println(listTest);
     }
 }
